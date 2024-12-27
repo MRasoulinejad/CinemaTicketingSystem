4ff4c8808f054fb86b77d097568543ce1016c6af
Moe Rasoulinejad <m.rasoulinejad@gmail.com>
2024-12-26 22:57:26 -0500

    web project updated

---
+++ CinemaTicketingSystem.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CinemaTicketingSystem.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
