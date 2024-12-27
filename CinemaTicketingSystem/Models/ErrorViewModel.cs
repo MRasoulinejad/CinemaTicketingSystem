59a1f812d02ebeccf572b60cca6ed6da0e4942af
Moe Rasoulinejad <m.rasoulinejad@gmail.com>
2024-12-26 22:58:58 -0500

    Delete CinemaTicketingSystem directory

--- CinemaTicketingSystem/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CinemaTicketingSystem.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
