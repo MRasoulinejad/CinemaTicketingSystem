3a28927512d095c8a84771004f89ab913f396acf
Moe Rasoulinejad <m.rasoulinejad@gmail.com>
2024-12-27 22:36:57 -0500

    MovieController Added

---
+++ CinemaTicketingSystem.Web/Controllers/MovieController.cs
@@ -0,0 +1,18 @@
+﻿using CinemaTicketingSystem.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaTicketingSystem.Web.Controllers
+{
+    public class MovieController : Controller
+    {
+        private readonly ApplicationDbContext _DbContext;
+        public MovieController(ApplicationDbContext DbContext)
+        {
+            _DbContext = DbContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
