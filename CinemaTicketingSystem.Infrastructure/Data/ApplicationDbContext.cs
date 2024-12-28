26676cf2b4e4af458cef8ebb8b632b4466b32578
Moe Rasoulinejad <m.rasoulinejad@gmail.com>
2024-12-27 21:58:46 -0500

    FixCascadeDeleteIssue - TablesAddedTo DB after adding a migration

--- CinemaTicketingSystem.Infrastructure/Data/ApplicationDbContext.cs
+++ CinemaTicketingSystem.Infrastructure/Data/ApplicationDbContext.cs
@@ -15,10 +15,33 @@ namespace CinemaTicketingSystem.Infrastructure.Data
         }
 
         public DbSet<Movie> Movies { get; set; }
-        public DbSet<ShowTime> ShowTimes { get; set; }
-        public DbSet<Payment> payments { get; set; }
-        public DbSet<Reservation> reservations { get; set; }
-        public DbSet<Seat> Seats { get; set; }
         public DbSet<Theatre> Theatres { get; set; }
+        public DbSet<Seat> Seats { get; set; }
+        public DbSet<ShowTime> ShowTimes { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Reservation>()
+                .HasOne(r => r.ShowTime)
+                .WithMany()
+                .HasForeignKey(r => r.ShowTimeId)
+                .OnDelete(DeleteBehavior.Restrict); 
+
+            modelBuilder.Entity<Reservation>()
+                .HasOne(r => r.Seat)
+                .WithMany()
+                .HasForeignKey(r => r.SeatId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+
+
+
+
+
     }
 }
