c2ab621b5d9e1d4c4221b373a15d33d7bcb16369
Moe Rasoulinejad <m.rasoulinejad@gmail.com>
2024-12-30 21:50:33 -0500

    Identity added to project

--- CinemaTicketingSystem.Web/Program.cs
+++ CinemaTicketingSystem.Web/Program.cs
@@ -1,4 +1,5 @@
 using CinemaTicketingSystem.Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
 
 var app = builder.Build();
 
